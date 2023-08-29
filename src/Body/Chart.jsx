affa74ecf104845963d4c5a2769c9582338ca4cb
Sergei Goroljov <s.goroljov@gmail.com>
2023-08-29 20:37:42 +0300

    !

--- src/Body/Chart.jsx
+++ src/Body/Chart.jsx
@@ -7,54 +7,20 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  ReferenceLine
 } from "recharts";
+import moment from 'moment';
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
-function Chart() {
+
+function Chart({electricityPrice }) {
+
+  const data = electricityPrice?.ee.map(
+    price => ({...price, hour:  moment.unix(price.timestamp).format('HH') })
+  );
+
+  const now = moment().format('HH');
+
   return (
   <div className="chartContainer">
     <ResponsiveContainer height="100%" width="100%">
@@ -68,17 +34,18 @@ function Chart() {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="hour" />
       <YAxis />
       <Tooltip />
       <Legend />
       <Line
         type="monotone"
-        dataKey="pv"
+        dataKey="price"
         stroke="#8884d8"
         activeDot={{ r: 8 }}
       />
       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      <ReferenceLine x={data?.findIndex(({hour}) => hour === now)}  stroke={'red'} />
     </LineChart>
     </ResponsiveContainer>
   </div>
--- src/Body/index.jsx
+++ src/Body/index.jsx
@@ -2,25 +2,48 @@ import { useState, useEffect } from "react";
 import Header from "./Header";
 import Chart from "./Chart";
 import DataTable from "./DataTable";
+import ErrorModal from "./ErrorModal";
 import {ELE , CHART} from '../constants';
 import "./Body.scss";
 import { getElectricityPrice, getGasPrice } from "../services/apiService";
+
+
 
 function Body({dataType, selectedPeriod}) {
 
   const [activeEnergy, setActiveEnergy] = useState(ELE);
   const [electricityPrice, setElectricityPrice] = useState(null);
   const [gasPrice , setGasPrice] = useState(null);
+  const [errorMessage , setErrorMessage] = useState(null);
+
+
+
 
   useEffect(() => {
-    getElectricityPrice(selectedPeriod).then(data => {
-      console.log(data);
-      setElectricityPrice(data.data);
-    })
-    getGasPrice(selectedPeriod).then(data => {
-      console.log('gas', data);
-      setGasPrice(data.data)
-    })
+      getElectricityPrice(selectedPeriod).then(data => {
+        console.log('ele' , data);
+        if(!data.success) {
+            throw data.messages[0];
+        }
+        setElectricityPrice(data.data);
+      })
+        .catch(setErrorMessage);
+
+
+
+      getGasPrice(selectedPeriod).then(data => {
+        console.log('gas', data);
+        if(!data.success) {
+          throw data.messages[0];
+        }
+        setGasPrice(data.data)
+      })
+      .catch(setErrorMessage);
+  
+      ;
+
+      
+    
   }, [selectedPeriod])
   
   return (
@@ -38,7 +61,7 @@ function Body({dataType, selectedPeriod}) {
          : <DataTable electricityPrice={electricityPrice}
          gasPrice={gasPrice}
          /> }
-       
+        <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)}/>
     </>
   );
 }

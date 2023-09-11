ff439c287d028728e92a06b03e3fc386ba24a62c
Sergei <124916772+aash3r@users.noreply.github.com>
2023-09-11 15:56:44 +0300

    Added contact form

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -1,7 +1,30 @@
-function Contact () {
-    return (
-        <></>
-    )
-};
+import { Button, Form } from 'react-bootstrap';
 
-export default Contact ;
+function Contact() {
+  return (
+    <Form className='mt-4'>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control required type='text' placeholder='Enter your name' />
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Email</Form.Label>
+        <Form.Control required type='email' placeholder='Enter your email' />
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicPassword'>
+        <Form.Label>Message</Form.Label>
+        <Form.Control
+          required
+          as='textarea'
+          rows={3}
+          placeholder='Enter your message'
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Contact;

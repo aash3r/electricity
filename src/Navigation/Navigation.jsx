99127ebf6ed2af3637d96152e64e66e2ab16af67
Sergei <124916772+aash3r@users.noreply.github.com>
2023-09-11 15:58:18 +0300

    Added new link to navigation

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,23 +1,30 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <Navbar bg="dark" data-bs-theme="light" className="bg-body-tertiary">
+    <Navbar bg='dark' data-bs-theme='light' className='bg-body-tertiary'>
       <Container>
-      <Link to="/" className="navbar-brand">
+        <Link to='/' className='navbar-brand'>
           <img
-            alt=""
-            src="/logo.svg"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
+            alt=''
+            src='/logo.svg'
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+          />{' '}
           React Bootstrap
-          </Link>
-        <Link className="nav-link" to="/contact">Contact</Link>
-        <Link className="nav-link" to="/gas">Gas</Link>
+        </Link>
+        <Link className='nav-link' to='/contact'>
+          Contact
+        </Link>
+        <Link className='nav-link' to='/gas'>
+          Gas
+        </Link>
+        <Link className='nav-link' to='/current-price'>
+          Current Price
+        </Link>
       </Container>
     </Navbar>
   );
